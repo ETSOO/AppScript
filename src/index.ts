c7751187fb11209dd78e784b945603a30c6ea325
Garry Xiao <xz@etsoo.com>
2022-10-19 19:28:03 +1300

    Upgrade dependencies

--- src/index.ts
+++ src/index.ts
@@ -33,10 +33,14 @@ export * from './erp/dto/ExchangeRateHistoryDto';
 export * from './erp/dto/IdLabelDto';
 export * from './erp/dto/IdLabelPrimaryDto';
 export * from './erp/dto/InitCallDto';
+export * from './erp/dto/OrgQueryDto';
 export * from './erp/dto/PublicProductDto';
 export * from './erp/dto/ResultPayload';
 
 // erp rq
+export * from './erp/rq/OrgListRQ';
+export * from './erp/rq/OrgQueryRQ';
+export * from './erp/rq/OrgRQ';
 export * from './erp/rq/QueryRQ';
 export * from './erp/rq/RefreshTokenRQ';
 export * from './erp/rq/TiplistRQ';
