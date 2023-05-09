63387c778045415fba490d18afe7742327341980
Garry Xiao <xz@etsoo.com>
2023-05-09 17:16:15 +1200

    Fix encrypt passphrase issue

--- src/app/CoreApp.ts
+++ src/app/CoreApp.ts
@@ -593,7 +593,7 @@ export abstract class CoreApp<
         this.passphrase = passphrase;
         this.storage.setData(
             this.fields.devicePassphrase,
-            this.encrypt(passphrase, this.name)
+            await this.encrypt(passphrase, this.name)
         );
 
         // Previous passphrase
