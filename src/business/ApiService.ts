ad32f298ed392af9f9a491060ed7a6e8f5d41599
Garry Xiao <xz@etsoo.com>
2023-06-12 12:15:50 +1200

    Add more ApiService items

--- src/business/ApiService.ts
+++ src/business/ApiService.ts
@@ -3,7 +3,32 @@
  */
 export enum ApiService {
     /**
-     * Amazon SES
+     * SMTP
+     * 邮件发送
      */
-    AmazonSES = 1
+    SMTP = 1,
+
+    /**
+     * SMTP delegation
+     * 邮件发送代理
+     */
+    SMTPDelegation = 2,
+
+    /**
+     * SMS
+     * 短信发送
+     */
+    SMS = 3,
+
+    /**
+     * Wechat service account
+     * 微信服务号
+     */
+    Wechat = 4,
+
+    /**
+     * Business Wechat
+     * 企业微信
+     */
+    WechatBusiness = 5
 }
