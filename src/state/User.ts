a764fbf993d5111fd0aaedfa12d38b5b36cf1db1
Garry Xiao <xz@etsoo.com>
2023-08-13 12:40:12 +1200

    Add new label changeReload

--- src/state/User.ts
+++ src/state/User.ts
@@ -11,31 +11,37 @@ export interface IUserData {
 
     /**
      * User name
+     * 用户姓名
      */
     readonly name: string;
 
     /**
      * User avatar
+     * 用户头像
      */
     readonly avatar?: string;
 
     /**
      * Organization id
+     * 机构编号
      */
     readonly organization?: number;
 
     /**
      * User role value
+     * 用户角色值
      */
     readonly role: number;
 
     /**
      * Refresh seconds
+     * 刷新间隔秒数
      */
     readonly seconds: number;
 
     /**
      * Access token
+     * 访问令牌
      */
     readonly token: string;
 }
