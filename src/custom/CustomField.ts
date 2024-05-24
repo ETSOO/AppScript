dd36e63f8c6a15d5abd27fb672d6bac435ef2b10
Garry Xiao <xz@etsoo.com>
2024-05-24 22:52:20 +1200

    Improve CustomField type

--- src/custom/CustomField.ts
+++ src/custom/CustomField.ts
@@ -4,33 +4,38 @@ import { CustomFieldData } from './CustomFieldData';
  * Custom field reference
  * 自定义字段引用
  */
-export type CustomFieldRef = {
+export type CustomFieldRef<V> = {
     /**
      * Get value
      */
-    getValue(): unknown;
+    getValue(): V;
 
     /**
      * Set value
      * @param value Value
      */
-    setValue(value: unknown): void;
+    setValue(value: V): void;
 };
 
 /**
  * Custom field props
  * 自定义字段属性
  */
-export type CustomFieldProps<D extends CustomFieldData> = {
+export type CustomFieldProps<D extends CustomFieldData, V> = {
     field: D;
-    onChange: (name: string, value: unknown) => void;
-    defaultValue?: unknown;
+    onChange: (name: string, value: V) => void;
+    defaultValue?: V;
 };
 
 /**
  * Custom field interface
  * 自定义字段接口
  */
-export interface ICustomField<D extends CustomFieldData, R> {
-    (props: CustomFieldProps<D>): R;
+export interface ICustomField<
+    V,
+    D extends CustomFieldData,
+    P extends CustomFieldProps<D, V>,
+    R
+> {
+    (props: P): R;
 }
